02577c3dc2e98f2b4e8f972af062b78498d2c118
23564 <707280427@qq.com>
2021-03-06 13:44:38 +0800

    2

--- pagesMy/pages/article/index.js
+++ pagesMy/pages/article/index.js
@@ -50,7 +50,7 @@ Page({
     })
 
     // let data = '<div><h3 style="color:red">javascript - <em>js同步编程</em>与异步编程的区别,异步有哪些优点,为什么...</h3><div><span>2016年5月20日 - </span>从编程方式来讲当然是<em>同步编程</em>的方式更为简单,但是同步有其局限性一是假如是单线程那么一旦遇到阻塞调用,会造成整个线程阻塞,导致cpu无法得到有效利用...</div><div><div></div><span ><span ></span></span> - 百度快照</div><div ><span>为您推荐：</span>js同步和异步ajax异步和同步的区别</div></div>';
-    this.setData({ nodes:data })
+   // this.setData({ nodes:data })
   },
 
   /**
